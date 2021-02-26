1a7e711ae45e4f2c9c4421e5b1e2a298e05a9b8e
unknown <purvi.harniya@gmail.com>
2021-02-26 15:51:39 +0530

    setup 1

---
+++ errorpages/404.php
@@ -0,0 +1,21 @@
+<?php
+header("HTTP/1.0 404 Not Found");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        error occured
+    </div>
+</body>
+
+</html>
