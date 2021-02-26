b1dafec5abf2f4bdee3a27ab0f4dc13c4bc6d739
unknown <purvi.harniya@gmail.com>
2021-02-26 16:20:20 +0530

    user update

--- admin/includes/topbar.php
+++ admin/includes/topbar.php
@@ -8,8 +8,6 @@
     <?php
 ?>
 
-
-
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
 
@@ -154,8 +152,7 @@
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
 
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $_SESSION['email']; ?></span>
-
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">NAME</span>
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
@@ -175,7 +172,6 @@
                 </a>
             </div>
         </li>
-
     </ul>
 
 </nav>
