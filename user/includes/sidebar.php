c659dd0cb19def547953a576b72254a58adf5b31
unknown <purvi.harniya@gmail.com>
2021-03-01 12:03:06 +0530

    user path setup

--- user/includes/sidebar.php
+++ user/includes/sidebar.php
@@ -164,10 +164,3 @@
 <div id="content-wrapper" class="d-flex flex-column">
     <!-- Main Content -->
     <div id="content">
-        <script src="../../vendor/jquery/jquery.min.js"></script>
-        <script src="../../vendor/js/sb-admin-2.min.js"></script>
-
-        <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-        <!-- Core plugin JavaScript-->
-        <script src="../../vendor/jquery-easing/jquery.easing.min.js"></script>
