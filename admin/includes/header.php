a3d57c6f51f2ef146471da1bd8639cd8c7315584
unknown <purvi.harniya@gmail.com>
2021-03-01 12:02:07 +0530

    path setup

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -10,17 +10,17 @@
     <meta name="author" content="">
     <title>Society Management System</title>
     <!-- Custom fonts for this template-->
-    <link href="../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
     <!-- Custom styles for this template-->
-    <link href="../../vendor/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="../vendor/css/sb-admin-2.min.css" rel="stylesheet">
     <!-- Custom styles for this page -->
-    <link href="../../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.bootstrap4.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/select/1.3.1/css/select.dataTables.min.css" rel="stylesheet">
-    <link href="../../vendor/css/fonts.css" rel="stylesheet">
+    <link href="../vendor/css/fonts.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 
--- admin/includes/scripts.php
+++ admin/includes/scripts.php
@@ -1,24 +1,24 @@
 <!-- Bootstrap core JavaScript-->
-<script src="../../vendor/jquery/jquery.min.js"></script>
-<script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../vendor/jquery/jquery.min.js"></script>
+<script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="../../vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="../../vendor/js/sb-admin-2.js"></script>
+<script src="../vendor/js/sb-admin-2.js"></script>
 
 <!-- Page level plugins -->
-<script src="../../vendor/chart.js/Chart.min.js"></script>
+<script src="../vendor/chart.js/Chart.min.js"></script>
 
 <!-- Page level custom scripts -->
-<script src="../../vendor/js/demo/chart-area-demo.js"></script>
-<script src="../../vendor/js/demo/chart-bar-demo.js"></script>
-<script src="../../vendor/js/demo/chart-pie-demo.js"></script>
-<script src="../../vendor/js/demo/chart-horizontal-demo.js"></script>
+<script src="../vendor/js/demo/chart-area-demo.js"></script>
+<script src="../vendor/js/demo/chart-bar-demo.js"></script>
+<script src="../vendor/js/demo/chart-pie-demo.js"></script>
+<script src="../vendor/js/demo/chart-horizontal-demo.js"></script>
 <!-- Page level plugins -->
-<script src="../../vendor/datatables/jquery.dataTables.min.js"></script>
-<script src="../../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.3.1/js/dataTables.buttons.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.3.1/js/buttons.html5.min.js"></script>
@@ -27,7 +27,7 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 
 <!-- Page level custom scripts -->
-<script src=" ../../vendor/js/demo/datatables-demo.js"> </script>
+<script src=" ../vendor/js/demo/datatables-demo.js"> </script>
 <script>
 // Add custom scripts here
 </script>
