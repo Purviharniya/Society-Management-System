e85c7ce591ce0bd023d75cf10b66845bb535ed28
Purviharniya <purvi.harniya@gmail.com>
2021-03-02 22:11:05 +0530

    minor updates

--- errorpages/401.php
+++ errorpages/401.php
@@ -37,7 +37,7 @@
 					<div class="container-fluid">
 						<!-- 404 Error Text -->
 						<div class="text-center">
-							<div class="error mx-auto" data-text="404">404</div>
+							<div class="error mx-auto" data-text="401">401</div>
 							<p class="lead text-gray-800 mb-5">
 								Authorization Error
 							</p>
