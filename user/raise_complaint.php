7a0623c5bc573b97417980870d75d8bdb05ffd08
unknown <purvi.harniya@gmail.com>
2021-03-19 11:23:11 +0530

    raise complaint UI

--- user/raise_complaint.php
+++ user/raise_complaint.php
@@ -12,6 +12,32 @@
             <div class="card shadow">
                 <div class="card-body">
                     <h4 class="card-title text-info mb-4">Raise a complaint</h4>
+                    <form action="./includes/queries/raise_complaint.php" method="POST" autocomplete="OFF">
+                        <div class="form-group">
+                            <label for="complaint_type">Complaint Type:</label>
+                            <select class="form-control" name="complaint_type" aria-describedby="typeHelp" required>
+                                <option selected value='0'>Choose a complaint type</option>
+                                <?php 
+                                
+                                $sql = "SELECT * from complainttypes";
+                                $res = mysqli_query($con,$sql);
+                                while($row = mysqli_fetch_array($res)){
+                                    echo "<option value= '" .$row['complaint_type']. "'>" . $row['complaint_type'] . "</option>"; 
+                                }
+                                ?>
+                            </select>
+                            <small id="typeHelp" class="form-text text-muted">Select the Complaint Type</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="complaint_desc">Complaint Description:</label>
+                            <textarea class="form-control" id="complaint_desc" name="complaint_desc"
+                                placeholder="Enter complaint description...." rows="5"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary" name='addcomplaint-btn'>Add</button>
+                        <button type="reset" class="btn btn-primary">Clear</button>
+
+                    </form>
                 </div>
             </div>
         </div>
