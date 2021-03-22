99b6fd62c2456ccc60a2a8cb839ca0128e6bfa82
unknown <purvi.harniya@gmail.com>
2021-03-22 16:58:02 +0530

    block no in login added

--- user/includes/handlers/login.php
+++ user/includes/handlers/login.php
@@ -2,18 +2,18 @@
 
 $_SESSION["otp"] = 0;
 
-if(isset($_POST["flatno"]) && isset($_POST["contactno"]))
+if(isset($_POST["flatno"]) && isset($_POST["contactno"]) && isset($_POST['blockno']))
 {
 $flatno=$_POST["flatno"];
 $contactno=$_POST["contactno"];
-
+$blockno = $_POST['blockno'];
 //echo $flatno;
 //echo $contactno;
 
 $db = mysqli_connect('localhost','root','','sms') or 
 die('Error connecting to MySQL server.');
 
-$query = "SELECT * FROM allotments WHERE FlatNumber= '{$flatno}'  ANd  ( ContactNumber = '{$contactno}' OR AlternateContactNo = '{$contactno}' ) ";
+$query = "SELECT * FROM allotments INNER join flats on allotments.FlatID=flats.FlatID WHERE allotments.FlatNumber='{$flatno}' AND ( allotments.ContactNumber = '{$contactno}' OR allotments.AlternateContactNo ='{$contactno}') and flats.BlockNumber='{$blockno}';";
 //echo $query;
 $result = mysqli_query($db, $query); 
 
@@ -30,7 +30,7 @@ $count=mysqli_num_rows($result);
         //session_start();
         $_SESSION["flatno"] =  $flatno;
         $_SESSION["contactno"] = $contactno;
-        
+        $_SESSION["blockno"]=$blockno;
         include("otp.php");
         
         
@@ -40,4 +40,4 @@ $count=mysqli_num_rows($result);
 
 mysqli_close($db);
 
-}?> 
+}?>
--- user/login.php
+++ user/login.php
@@ -1,9 +1,14 @@
-
 <?php
 include("user/includes/handlers/login.php");
 ?>
 
 <form method="POST" class="userForm p-3" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <div class="form-group">
+        <div class="input-group">
+            <span><i class="fa fa-building"></i></span>
+            <input type="text" class="form-control" name="blockno" id="blockno" placeholder="Enter your Block Number">
+        </div>
+    </div>
     <div class="form-group">
         <div class="input-group">
             <span><i class="fa fa-building"></i></span>
