7355ef3fe3c90aefd68da9761d22fa12d7865ea6
Jill-S <jill25@somaiya.edu>
2021-03-28 16:59:45 +0530

    changes

--- admin/manage_allotments.php
+++ admin/manage_allotments.php
@@ -1,8 +1,8 @@
 <?php
 include '../config.php';
-include './includes/header.php';
-include './includes/sidebar.php';
-include './includes/topbar.php';
+include 'includes/shared/header.php';
+include 'includes/shared/sidebar.php';
+include 'includes/shared/topbar.php';
 ?>
 <div class='container-fluid'>
     <h3 class="my-4">Allotments</h3>
