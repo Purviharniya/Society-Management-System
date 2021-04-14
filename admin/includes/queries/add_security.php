3fa014d4eb935353e1a2038c97215515d0587c8c
unknown <purvi.harniya@gmail.com>
2021-04-14 17:56:13 +0530

    changes

--- admin/includes/queries/add_security.php
+++ admin/includes/queries/add_security.php
@@ -22,7 +22,7 @@ if (isset($_POST['addsecurity-btn'])) {
 
     } else {
         // store in the database; check if error doesnt occur while storing
-        $query = "INSERT INTO security(`SecurityID`, `Name`, `ContactNumber`, `Shift`,`created_at`,`updated_at`) VALUES ('$securityid' , '$name' , '$contactnumber' , '$shift' , '$timestamp','' )";
+        $query = "INSERT INTO security(`SecurityID`, `Name`, `ContactNumber`, `Shift`,`created_at`,`updated_at`) VALUES ('$securityid' , '$name' , '$contactnumber' , '$shift' , '$timestamp','$timestamp' )";
         mysqli_query($con, $query);
 
         //Start the session if already not started.
@@ -35,51 +35,4 @@ if (isset($_POST['addsecurity-btn'])) {
 }
 
 // :)
-
-if (isset($_POST['delete_flatarea'])) {
-    $recordID = mysqli_escape_string($con, $_POST['record_id']);
-    $sql = "DELETE FROM flatarea WHERE FlatAreaID='$recordID'";
-    mysqli_query($con, $sql);
-    // header("Location: ../bla.php");
-    exit();
-}
-
-if (isset($_POST['update_flatarea'])) {
-    // echo "hi";
-    $block_new = mysqli_escape_string($con, $_POST['blockno_new']);
-    $series_new = mysqli_escape_string($con, $_POST['series_new']);
-    $area_new = mysqli_escape_string($con, $_POST['area_new']);
-    $rate_new = mysqli_escape_string($con, $_POST['rate_new']);
-    $recordID = mysqli_escape_string($con, $_POST['recordID']);
-    $flattype_new = mysqli_escape_string($con, $_POST['flattype_new']);
-    // $added_by = 'Admin1';
-    $added_by = $_SESSION['username'];
-    $timestamp = date("Y-m-d H:i:s");
-    $block_old = mysqli_escape_string($con, $_POST['blockno_old']);
-    $series_old = mysqli_escape_string($con, $_POST['series_old']);
-
-    // if the admin is changing unique value constraints, we check if they already exist or not
-    if (($block_new != $block_old) || ($series_new != $series_old)) {
-
-        $check_query = "SELECT * from flatarea where BlockNumber='$block_new' AND FlatSeries='$series_new';";
-        // echo $check_query;
-        $check_result = mysqli_query($con, $check_query);
-        if (mysqli_num_rows($check_result) != 0) {
-            echo "Exists_record";
-        } else {
-            $sql = "UPDATE flatarea SET BlockNumber='$block_new', FlatSeries='$series_new',FlatArea='$area_new',FlatType='$flattype_new',Ratepsq='$rate_new',Updatedby='$added_by',UpdatedAt='$timestamp' WHERE FlatAreaID='$recordID';";
-            // echo $sql;
-            mysqli_query($con, $sql);
-            exit();
-        }
-    }
-    //and agar exist nahi karta toh it will be unique and we can directly update
-    else {
-        $sql = "UPDATE flatarea SET BlockNumber='$block_new', FlatSeries='$series_new',FlatArea='$area_new',FlatType='$flattype_new',Ratepsq='$rate_new',Updatedby='$added_by',UpdatedAt='$timestamp' WHERE FlatAreaID='$recordID';";
-        // echo $sql;
-        mysqli_query($con, $sql);
-        exit();
-    }
-
-}
-// }
+//}
